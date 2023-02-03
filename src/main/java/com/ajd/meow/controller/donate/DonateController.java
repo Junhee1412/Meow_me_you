895cc03760258b37d1bb0c6e742ea9a95a96a507
준희 <118590361+Junhee1412@users.noreply.github.com>
2023-02-03 18:05:43 +0900

    feat : 후원하기 update, delete 기능 추가 확정 채팅기능 추가

--- src/main/java/com/ajd/meow/controller/donate/DonateController.java
+++ src/main/java/com/ajd/meow/controller/donate/DonateController.java
@@ -7,6 +7,10 @@ import com.ajd.meow.repository.donate.CreditcardRepository;
 import com.ajd.meow.repository.donate.DonateRepository;
 import com.ajd.meow.service.donate.DonateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -43,7 +47,6 @@ public class DonateController {
 
     @GetMapping("/donatesuccess.meow")
     public String donateSuccess(HttpSession session, Model model){
-
         UserMaster loginUser=(UserMaster)session.getAttribute("user");
         model.addAttribute("user",loginUser);
 
@@ -61,8 +64,8 @@ public class DonateController {
     public String donate(HttpServletRequest request, HttpSession session, DonateMaster donateMaster,
                          BankTransfer bankTransfer, CreditcardPayment creditcardPayment, AccountTransfer accountTransfer,
                          RedirectAttributes redirectAttributes,Model model){
-        //신용카드 한도초과, 계좌이체 잔액부족일 경우는 결제 API를 구현하지 않을 예정이기 때문에 주석으로 코드만 작성
 
+        //신용카드 한도초과, 계좌이체 잔액부족일 경우는 결제 API를 구현하지 않을 예정이기 때문에 주석으로 코드만 작성
         UserMaster loginUser=(UserMaster)session.getAttribute("user");
         model.addAttribute("user",loginUser);
 
@@ -126,8 +129,36 @@ public class DonateController {
     }
 
     @GetMapping("/donatelist.meow")
-    public String donatelistForm(Model model){
+    public String donatelistForm(@PageableDefault(page = 0,size = 10, sort = "donateCode", direction = Sort.Direction.DESC) Pageable pageable, HttpSession session, Model model){
+        UserMaster loginUser=(UserMaster)session.getAttribute("user");
+        model.addAttribute("user",loginUser);
+
         model.addAttribute("list", donateservice.donateList());
+
+        Page<DonateMaster> lists = donateservice.donateList(pageable);
+        int nowPage = lists.getPageable().getPageNumber()+1 ;
+        int startPage = Math.max(0 , 1);
+
+        int endPage = Math.min(nowPage + 10 , lists.getTotalPages());
+
+        model.addAttribute("list", lists);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("maxPage",10);
+
         return "spon_list";
+    }
+
+    @GetMapping("/donatedelete.meow")
+    public String donateDelete(Long donateCode){
+        donateservice.deleteDonate(donateCode);
+        return "redirect:/donatelist.meow";
+    }
+
+    @GetMapping("/donateconfirm.meow")
+    public String donateConfirm(Long donateCode) {
+        donateservice.confirmDonate(donateCode);
+        return "redirect:/donatelist.meow";
     }
 }

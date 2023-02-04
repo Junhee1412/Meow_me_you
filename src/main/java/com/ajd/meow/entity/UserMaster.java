bc5f42e04300614a722edf099d47266f346d567d
준희 <118590361+Junhee1412@users.noreply.github.com>
2023-02-04 23:41:33 +0900

    feat : 기부금 영수증(상세보기)

--- src/main/java/com/ajd/meow/entity/UserMaster.java
+++ src/main/java/com/ajd/meow/entity/UserMaster.java
@@ -7,8 +7,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 //논리명 사용자정보
 @Entity
@@ -61,18 +59,12 @@ public class UserMaster {
     @Column(name = "INTRODUCE")
     private String introduce;
 
-    @Column(name = "USER_PRF")
-    private String userProfile;
-
     @Column(name = "PRF_IMG_NAME")
     private String profileImageName;
 
-    @Column(name = "PRF_IMG_SIZE")
-    private String profileImageSize;
+    @Column(name = "PRF_IMG_PATH")
+    private String profileImagePath;
 
     @Column(name = "USER_JOIN_DATE")
     private LocalDateTime userJoinDate;
-
-//    @OneToMany(mappedBy = "userMaster")
-//    private List<Reply> replyList = new ArrayList<>();
 }
